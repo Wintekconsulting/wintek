05d65a26e8538ea47a97ca8353cf7616ebf83c48
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2023-07-08 21:37:46 +0200

    pagina vive ok

--- src/pages/vive-la-experiencia-wintek/index.jsx
+++ src/pages/vive-la-experiencia-wintek/index.jsx
@@ -5,35 +5,30 @@ import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import styles from './Vive.module.css';
+import { InputGroup } from 'react-bootstrap';
 
 function Vive() {
   return (
     <div>
 
         <NavWintek/>
-        <div className={styles.container}>
-        <p className={styles.title}>VIVE LA EXPERINCIA WINTEK</p>
+        <div className="container">
+        <p className="title">VIVE LA EXPERINCIA WINTEK</p>
         <Card className={styles.containerSection}>
         <Card.Body classname={styles.cardBody}>
-            <p className={styles.text}> Si quieres iniciar tu carrera o continuar tu trayectoria profesional en una de las empresas de transformación digital más innovadoras, envíanos tu hoja de vida para descubrir tu perfil.</p>
-            <p className={styles.text}> Queremos conocerte y trabajar de la mano para crecer juntos. Vive la experiencia Wintek y sé parte de nuestra historia.</p>
-            <Form className='cont-form'>
-              <Form.Group as={Row} className="mb-3" controlId="formArchivo">
-              <Row>
-                <Form.Label className={styles.textLabel} column="lg" lg={2}>Adjuntar archivo</Form.Label>
-                <Col>
-                  <Form.Control size="lg" type="file"/>
-                </Col>
-              </Row>
-              </Form.Group>
-              
-              <Form.Group as={Row} className="mb-3">
-                <Col sm={{ span: 10, offset: 2 }}>
-                    <Button className={styles.button} type="submit">Enviar archivo</Button>
-                </Col>
-                </Form.Group>
-              </Form>
-         
+            <p className="text"> Si quieres iniciar tu carrera o continuar tu trayectoria profesional en una de las empresas de transformación digital más innovadoras, envíanos tu hoja de vida para descubrir tu perfil.</p>
+            <p className="text"> Queremos conocerte y trabajar de la mano para crecer juntos. Vive la experiencia Wintek y sé parte de nuestra historia.</p>
+
+            <Form className={styles.containerSection}>
+            <InputGroup className="mb-3">
+              <Form.Control type="file"/>
+            </InputGroup>
+            
+            <p className="text">Envíanos tu  currículum</p>
+            
+            <Button className= {styles.button} type="submit">Enviar</Button>
+            </Form>
+        
         </Card.Body>
         </Card>
 
