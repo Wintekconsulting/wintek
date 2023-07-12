c1282fa2ae8fdd2060ff20ad133a7ac8c2348007
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2023-07-12 16:08:29 +0200

    media queries ajustadas still missing

--- src/components/blogCard/BlogCard.jsx
+++ src/components/blogCard/BlogCard.jsx
@@ -8,14 +8,14 @@ function BlogCard({ image, title, date, linkPage, group, author }) {
       <Card style={{ width: '20rem'}} className={style.cardStyle}>
         <Image variant="top" src={image} width={100} height={100} alt="image from blog"/>
         <Card.Body>
-          <Card.Title>{title}</Card.Title>
-          <Card.Text>{date}</Card.Text>
+          <Card.Title className={style.title}>{title}</Card.Title>
+          <Card.Text className={style.smallText}>{date}</Card.Text>
           <a href={linkPage}>
-            <Button variant="primary" className="bttnSection">Leer más</Button>
+            <Button className={style.button}>Leer más</Button>
           </a>
-          <Card.Text>{group}</Card.Text>
+          <Card.Text className={style.mediumText}>{group}</Card.Text>
           <Card.Footer>
-          <small className="text-muted">{author}</small>
+          <small className={`text-muted mediumText`}>{author}</small>
         </Card.Footer>
         </Card.Body>
       </Card>
